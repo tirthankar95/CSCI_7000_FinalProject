8c0b5a64e0799e03a49f97a5909add1d195e4ca2
ajaymopidevi <ajay@Ajay.localdomain>
2022-11-27 10:54:05 -0700

    Adding MiniGrid files

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup
 
 setup(
-    name="gym-examples",
-    version="0.0.1",
-    install_requires=["gym==0.26.1", "pygame==2.1.0"],
+    name='gym_minigrid',
+    version='0.0.5',
+    keywords='memory, environment, agent, rl, openaigym, openai-gym, gym',
+    url='https://github.com/maximecb/gym-minigrid',
+    description='Minimalistic gridworld package for OpenAI Gym',
+    packages=['gym_minigrid', 'gym_minigrid.envs'],
+    install_requires=[
+        'gym>=0.9.6',
+        'numpy>=1.15.0',
+        'pyqt5>=5.10.1'
+    ]
 )
